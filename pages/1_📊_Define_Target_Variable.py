26e569ed8a7a446f4b5842a37d9b2ee608021563
Dalia Gala <dalia.gala@merton.ox.ac.uk>
2023-05-26 13:47:36 +0100

    adding revised text

--- pages/1_📊_Define_Target_Variable.py
+++ pages/1_📊_Define_Target_Variable.py
@@ -22,17 +22,17 @@ dataframe = dataframe.rename(columns={"education_level": "education level"})
 groups = ["attention", "reasoning", "memory", "behavioural restraint", "information processing speed"]
 
 groups_dict = {
-          'divided_visual_attention' : 'attention',
-          'forward_memory_span': 'memory',
-          'arithmetic_problem_solving' : 'reasoning',
-          'logical_reasoning' : 'reasoning',
-          'adaptive_behaviour_response_inhibition' : 'behavioural restraint',
-          'reverse_memory_span' : 'memory',
-          'episodic_verbal_learning' : 'memory',
-          'delayed_recall' : 'memory',
-          'abstract_symbol_processing_speed' : 'information processing speed',
-          'numerical_info_processing_speed' : 'information processing speed',
-          'numerical_and_lexical_info_processing_speed' : 'information processing speed'
+          'Divided Visual Attention' : 'attention',
+          'Forward Memory Span': 'memory',
+          'Arithmetic Reasoning' : 'reasoning',
+          'Grammatical Reasoning' : 'reasoning',
+          'Go/No go' : 'behavioural restraint',
+          'Reverse_Memory_Span' : 'memory',
+          'Verbal List Learning' : 'memory',
+          'Delayed Verbal List Learning' : 'memory',
+          'Digit Symbol Coding' : 'information processing speed',
+          'Trail Making Part A' : 'information processing speed',
+          'Trail Making Part B' : 'information processing speed'
         }
 
 education_dict = {
@@ -51,79 +51,44 @@ education_dict = {
 
 ### CREATE THE "TARGET VARIABLE DEFINITION" PAGE ###
 st.title('Target variable definition')
+st.markdown('''Pretend you’re hiring for a position of your choice. Your task below is to define **two different notions** of what a 
+             successful employee for that position is by assigning different weights to characteristics. We have pre-filled the
+             choices to reflect a manager who values attentiveness and numerical skills (A) and a manager who values interpersonal 
+             skills and memory (B). If you want to, change the slider values and click :red[“Assign labels and train your models”]. The 
+             dashboard will add class labels "1" and "0" based on your selections,a dn generate two different models, one for each of your 
+             target variable definitions. In the “See visualizations” page, you can see how your two models differ in matters of bias and 
+             overall performance.''')
+
 
 with st.expander("See explanation"):
-    st.markdown('''On this page, you can participate in the process of defining the target variable for a hiring model. The :green[target variable is 
-    the variable, or a changeable quality, whose value is defined and predicted by other variables]. In the case of a hiring model, the target variable
-    usually is: who should be hired? Or: who should be interviewed? Therefore, the target variable will be **a group of people**. Those to be selected will be
-    labelled "1", and those who are not to be selected, will be labelled "0". This is an example of a **classification task** in machine learning. Once these 
-    groups are defined in some way, we can show the model some examples of "features" which determine who should belong to which group.''')
+    st.markdown('''On this page, you can define two different target variables for a hiring algorithm: each target variable is defined in terms of the cognitive characteristics that one might think a successful employee has. Once you define the two target variables, you click “Assign labels and train your models” and the dashboard will generate two different models, one for each of your target variable definitions. In the “See visualizations” page, you can see how your two datasets and models differ in matters of bias and overall performance.''')
     
-    st.markdown('''In a real-world scenario,the target variable would be defined by a hiring manager based on their **implicit assessment** of current employees. 
-    The hiring manager would decide who she thinks performs well, and who does not, in her company. She would then ask those employees to play 
-    the cognitive games for the purpose of generating features based on which the hiring algorithm can be trained. The algorithm would know who was selected to 
-    have the label "1", and who was selected to have the label "0" because that was what the hiring manager decided. But if another hiring manager was making this 
-    decision on a different occasion, he might have a **different baseline** with respect to which he would select those employees. Both hiring managers therefore 
-    :green[implicitly assign different importance to different characteristics of incumbent employees:] how good they are at presentations, how quickly they complete tasks, 
-    how good their memory is and how quickly they learn new things, or similar. Finally, when new applicants apply for a job in this company, they will also be 
-    asked to play the same games. If the applicants' results are similar to those achieved by the top employees, the model might select them for interview or hiring.
-    They will be labelled "1" by the model. If not, the model will not select them. They will be labelled "0".''')
+    st.markdown('''The kind of models that the dashboard builds are of a kind increasingly used in hiring software: companies will have applicants play games that test for different kinds of cognitive ability (like reasoning or information-processing speed). And then hiring software will be built to predict which applicants will be successful based on which cognitive characteristics they have. What cognitive characteristics make for a successful employee? This will depend on what role is being hired for (salesperson? engineer? - the choice is yours). And it will also depend on how we define “successful employee.”''')
     
-    st.markdown('''Therefore, it is easy to imagine and understand that the way in which the hypothetical hiring manager selects the group of top employees will have a 
-    :green[large effect on who the model learns to select]. One manager might favour attentiveness and numerical skills more, while another might be more focused
-    on interpersonal skills and having good memory. When these two groups of people play the games, their results, and what the model learns to detect, 
-    will differ. Not only that; :green[the demographics selected by these models will most likely be very different, too.]''')
+    st.markdown('''In the real-world, “successful employee” is defined for these kinds of hiring models in the following way. Managers from the company doing the hiring will select a group of current employees that they consider to be successful; this group of current employees will play the cognitive test games. The hiring algorithm will then try to identify applicants who are similar—in cognitive characteristics—to the current employees that are considered successful. The target variable of “successful employee” is thus defined in terms of comparison to certain people who are deemed successful. ''')
 
-    st.markdown('''Here, :green[**you can define the target variable**] for two hiring models yourself in an **explicit** manner, and see the effect that these varying definitions have on the target demographics 
-    of your models. Rather than weighing different features of hypothetical employees in your head, and deciding "1", or "0", you will do so numerically. To do that, we preprocessed and normalised
-    data from battery 26 of the [**NCPT dataset**](https://www.nature.com/articles/s41597-022-01872-8) which is a dataset with results of cognitive performance tests. We selected this battery because
-    it had the most subtests from all batteries in the NCPT dataset, 11 subtests, and therefore, most available features to use. We treat the results of each of the subtests as features for a hiring model,
-    and we group them into 5 categories based on what the subtests measure:''')
-
-    c1, c2, c3, c4, c5 = st.columns((2.5,3,2,1,2))
-    with c1:
-      st.markdown(
+    st.markdown('''One will get different target variables if one deems different current employees as the successful ones. And, as we discussed in the About page (and as we explain more in the Putting the Idea Into Practice), there will likely be disagreement between managers about which employees are successful. For instance, a manager who values attentiveness and numerical skills will deem different employees “successful” in comparison to a manager who values interpersonal skills and memory. Even when different managers roughly share their sensibilities in what characteristics make for a successful employee, there may still be different, equally good ways to “weight” the importance of the various characteristics.''')
+    
+    st.markdown('''In the real-world, the cognitive characteristics shared by those considered successful employees is implicit. Companies do not first identify what cognitive characteristics make for a successful employee; rather, they identify employees who they consider successful, and then the hiring model “works backwards” to identify what characteristics these employees share.''')
+    
+    st.markdown('''In our dashboard, these cognitive characteristics are explicit. You pick—using the sliding scales—which cognitive characteristics you think are more or less important to a successful employee. (You can imagine that you are hiring for a specific role.) You’ll do this once to define “successful employee” in one way, and then a second time to define “successful employee” in another way. (We’ve made the cognitive characteristics explicit both so you can see the point of different target variable definitions more clearly, and because of limitations of the data that we’re working with; see “Methodology” for more.)''')
+    
+    st.markdown('''The cognitive characteristics that you can give different weights are taken from one of the datasets from the NeuroCognitive Performance Test ([**NCPT dataset**](https://www.nature.com/articles/s41597-022-01872-8)). This dataset has eleven different tests which can be grouped into the following broader categories:''')
+    
+    st.markdown(
           """
-          **Memory**:
-          - forward memory span
-          - reverse memory span
-          - episodic verbal learning
-          - delayed recall
-          """)
-    with c2:
-      st.markdown(
-          """
-          **Information processing speed**:
-          - abstract symbol processing speed
-          - numerical info processing speed
-          - numerical and lexical info processing speed
-          """)
-    with c3:
-      st.markdown(
-          """
-          **Reasoning**:
-          - arithmetic problem solving 
-          - logical reasoning
-          """)
-    with c4:
-      st.markdown(
-          """
-          **Attention**:
-          - divided visual attention
-          """)
-    with c5:
-      st.markdown(
-          """
-          **Behavioural restraint**:
-          - adaptive behaviour response inhibition
+          - **Memory**: Forward Memory Span, Reverse Memory Span, Verbal List Learning, Delayed Verbal List Learning
+          - **Information Processing Speed**: Digit Symbol Coding, Trail Making Part A, Trail Making Part B
+          - **Reasoning**: Arithmetic Reasoning, Grammatical Reasoning
+          - **Attention**: Divided Visual Attention
+          - **Behavioral Restraint**: Go/No go
           """)
     
-    st.markdown('''Below, you can see sliders corresponding to these 5 categories, and thus, the characteristics of hypothetical employees. You can assign weights to these features by changing the slider values, and these
-    weights will be translated into the importance of these features when assigning a score to the hypothetical employee. Once you are finished setting the slider values, you can see what % values you assigned to 
-    each subtest by ticking the checkbox below the sliders. When you are finished with your selections, press "Assign values and train your models" button. Scores will be assigned to each person in the dataset 
-    based on your selection. These scores will be different for models A and B if you selected varying slider values for the characteristics which are used as features in these models. In the top 15% of candidates
-    based on each score for each model A and B, 100 candidates will be labelled "1", and the rest of the candidates in the dataset will have the label "0". The candidates will be labelled "1" with increasing probability 
-    of being selected the higher their score was. This is done to emulate what a real-world decision might look like, and to introduce noise to the model and prevent it from being 100% accurate.''')
+    st.markdown('''You can assign weights to these features using sliders. The weights will represent the importance of these features in defining a “successful employee.”''')
+    
+    st.markdown('''You can check the assigned percentages for each subtest by ticking the checkbox beneath the sliders. Once you're done, press "Assign values and train your models". Different scores will be assigned to each individual in the dataset based on your input, and these scores will differ between models if you've selected different slider values.''')
+    
+    st.markdown('''After this, the models will be trained, and you'll see output metrics for each model, including shared candidates between datasets, model accuracy and its confidence interval, confusion matrices, and other model metrics. Finally, you can explore demographic differences between your models under "Visualize your models" in the sidebar.''')
 
 col1, col2 = st.columns(2)
 
