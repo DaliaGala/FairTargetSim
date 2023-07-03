5d9689400cdcae0e78a554ca66450f6629d3c2b1
Dalia Gala <dalia.gala@merton.ox.ac.uk>
2023-07-03 12:28:58 +0100

    Changing github icon

--- 🏠Home.py
+++ 🏠Home.py
@@ -6,12 +6,6 @@ Created on Thu May 25 15:13:58 2023
 @author: daliagala
 """
 
-hide_github_icon = “”"
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-
-“”"
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 ### LIBRARIES ###
 import streamlit as st
 
